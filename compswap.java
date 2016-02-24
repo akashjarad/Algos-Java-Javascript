91eb4182a0c195cdbd456adf5a882d8d24df4451
jrisk <joeyrsk@gmail.com>
2016-02-23 18:34:36 -0600

    java swap example

---
+++ compswap.java
@@ -0,0 +1,12 @@
+private static boolean less(Comparable v, Comparable w) {
+	
+	return (v.CompareTo(w) < 0)
+}
+
+private static void exch(Comparable[] a, int i, int j) {
+	Comparable swap = a[i];
+
+	a[i] = a[j];
+
+	a[j] = swap;
+}
