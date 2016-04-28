8ad2fb677fe93bdbf8c6cf4447b781c0d69f343f
jrisk <joeyrsk@gmail.com>
2016-04-28 13:11:00 -0400

    finish Assignment 1

--- Percolation.java
+++ Percolation.java
@@ -1,60 +1,83 @@
-import myclasses.Maker;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.util.*;
-import myclasses.UF;
+
 
 public class Percolation {
 
-	public static void main(String[] args) {
+	private WeightedQuickUnionUF percArray;
+	private boolean[] mapOpen;
+	private int N = 0;
+	private int top = 0;
+	private int bottom;
+	private int size;
 
-		boolean p = false;
+	public Percolation(int N) {
+		mapOpen = new boolean[N*N];
+		percArray = new WeightedQuickUnionUF(N * N + 2);
+		bottom = N*N+1;
+		size = N;
+	}
 
-		int i = 0;
-
-		Maker gridT = new Maker();
-
-		UF unionizer = new UF(25);
-
-		System.out.println(unionizer.count());
-
-		int grid[] = gridT.MakerFun(5,5);
-
-		for (i = 0; i < grid.length; i++) {
-			if (i % 5 == 0)
-				System.out.print("\n[ ]");
-			else
-				System.out.print("[ ]");
-		} 
-
-		//need isOpen method
-
-		while (p == false) {
-
-			double rando = Math.floor(Math.random() * 100); //needs to run until all are filled or it percolates
-
-			int randoInt = (int)rando;
-
-			if (randoInt < grid.length) {
-
-			grid[randoInt] = 101; //101 or true to connect values meaing "on" or "filled"
-
-			if (randoInt > 0 && grid[randoInt-1] == 101) {
-				unionizer.union(grid[randoInt], grid[randoInt-1]);
-				}
-			else if (randoInt < 24 && grid[randoInt+1] == 101 && randoInt % 5 != 0) {
-				unionizer.union(grid[randoInt], grid[randoInt+1]);
-			}
-			else if (randoInt < 20 && grid[randoInt+5] == 101) {
-				unionizer.union(grid[randoInt], grid[randoInt+5]);
-			}
-			else if (randoInt > 4 && grid[randoInt-5] == 101) {
-				unionizer.union(grid[randoInt], grid[randoInt-5]);
+	public void open(int i, int j) {
+		if (i < 1 || i > size || j < 1 || j > size) {
+			throw new java.lang.IndexOutOfBoundsException();
 			}
 
+		mapOpen[(i-1) * N + (j-1)] = true;
 
-			System.out.println(randoInt);
-
-			p = true;
-			}
+		if (i == 1) {
+			percArray.union(getQFIndex(i,j), top);
 		}
+
+		if (i == size) {
+			percArray.union(getQFIndex(i,j), bottom);
+		}
+
+		if (j > 1 && isOpen(i, j - 1)) { // left
+           percArray.union(getQFIndex(i, j), getQFIndex(i, j - 1));
+        }
+        if (j < size && isOpen(i, j + 1)) { // right
+            percArray.union(getQFIndex(i, j), getQFIndex(i, j + 1));
+        }
+        if (i > 1 && isOpen(i - 1, j)) { // bottom
+            percArray.union(getQFIndex(i, j), getQFIndex(i - 1, j));
+        }
+        if (i < size && isOpen(i + 1, j)) { //top
+            percArray.union(getQFIndex(i, j), getQFIndex(i + 1, j));
+        }
+	}
+
+	public int getQFIndex(int i, int j) {
+		return (i - 1) * N + j;
+	}
+
+	public boolean percolates() {
+		return percArray.connected(top, bottom);
+	}
+
+	public boolean isOpen(int i, int j) {
+		return (mapOpen[(N * (i-1)) + (j-1)] == true);
+	}
+
+	public boolean isClosed(int i, int j) {
+		return (mapOpen[(N * (i-1)) + (j-1)] != true);
+	}
+
+	public boolean isFull(int i, int j) {
+		if (0 < i && i <= size && 0 < j && j <= size) {
+            return percArray.connected(top, getQFIndex(i,j));
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+	}
+
+	public static void main(String[] args) {
+		Percolation whatever = new Percolation(5);
+
+		whatever.open(2,4);
+
+		System.out.println(whatever.isOpen(2,4));
+
+		whatever.isOpen(2,5);
 	}
 }
